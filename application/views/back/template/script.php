2aff935e48e7469ad588f17364e331566090a9a2
Putu Widyantara <putuwaw973@gmail.com>
2023-05-14 23:15:41 +0800

    [fix] missing display at dashboard

---
+++ application/views/back/template/script.php
@@ -0,0 +1,39 @@
+<!-- jQuery -->
+<script src="assets/back/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="assets/back/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+	$.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="assets/back/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="assets/back/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="assets/back/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="assets/back/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="assets/back/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="assets/back/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="assets/back/plugins/moment/moment.min.js"></script>
+<script src="assets/back/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="assets/back/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="assets/back/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="assets/back/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/back/dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets/back/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="assets/back/dist/js/demo.js"></script>
+
+
+</body>
+
+</html>
